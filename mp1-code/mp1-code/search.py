11c4f827d98d96bb4417fa581f3cc4cab987df27
Sean Yao <ckyao2@illinois.edu>
2020-09-04 18:30:53 +0800

    Astar Done

--- mp1-code/mp1-code/search.py
+++ mp1-code/mp1-code/search.py
@@ -36,67 +36,75 @@ def bfs(maze):
     @return path: a list of tuples containing the coordinates of each state in the computed path
     """
     # TODO: Write your code here
-    from collections import deque
-
-    queue = deque([])
-    visited = []
-    predecessor = dict()
-    path = []
-
-    start_pos = maze.getStart()
-
-    queue.append(start_pos)
-    predecessor[start_pos] = None
-
-    while queue:
-        cur_pos = queue.popleft()
-        cur_row, cur_col = cur_pos
-        visited.append(cur_pos)
-
-        if cur_pos in maze.getObjectives():
-            break
-
-        for next_pos in maze.getNeighbors(cur_row, cur_col):
-            if next_pos in visited:
-                continue
-            predecessor[next_pos] = cur_pos
-            queue.append(next_pos)
-
-    while cur_pos != None:
-        path.append(cur_pos)
-        cur_pos = predecessor[cur_pos]
-    path.reverse()
-
-    return path
     # from collections import deque
 
     # queue = deque([])
     # visited = []
+    # predecessor = dict()
     # path = []
 
-    # path.append(maze.getStart())
-    # queue.append(path)
-    # counter = 0
+    # start_pos = maze.getStart()
 
-    # while queue and counter < 1000:
-    #     counter = counter + 1
-    #     cur_path = queue.popleft()
-    #     cur_pos = cur_path[len(cur_path)-1]
-    
+    # queue.append(start_pos)
+    # predecessor[start_pos] = None
+
+    # while queue:
+    #     cur_pos = queue.popleft()
     #     cur_row, cur_col = cur_pos
-
     #     visited.append(cur_pos)
 
     #     if cur_pos in maze.getObjectives():
-    #         print(cur_path)
-    #         return cur_path
+    #         break
 
-    #     else:
-    #         for next_pos in maze.getNeighbors(cur_row,cur_col):
-    #             if next_pos in visited:
-    #                 continue
-    #             temp_path = cur_path + [next_pos]
-    #             queue.append(temp_path)
+    #     for next_pos in maze.getNeighbors(cur_row, cur_col):
+    #         if cur_pos not in queue:
+    #             predecessor[next_pos] = cur_pos
+    #             queue.append(next_pos)
+
+    # while cur_pos != None:
+    #     path.append(cur_pos)
+    #     cur_pos = predecessor[cur_pos]
+    # path.reverse()
+
+    # return path
+    
+    print(maze.getObjectives())
+
+    queue = []
+    visited = []
+    predecessor = dict()
+
+    start_pos = maze.getStart()
+
+    queue.append(maze.getStart())
+    predecessor[start_pos] = None
+    counter = 0
+
+    while queue and counter < 1000:
+        counter = counter + 1
+        cur_pos = queue.pop(0)
+        # print(predecessor)
+
+        visited.append(cur_pos)
+
+    
+        cur_row, cur_col = cur_pos
+
+
+        if cur_pos in maze.getObjectives():
+            path = []
+            while cur_pos != None:
+                path.append(cur_pos)
+                cur_pos = predecessor[cur_pos]
+            path.reverse()
+            return path
+        else:
+            for next_pos in maze.getNeighbors(cur_row,cur_col):
+                
+                if next_pos in visited:
+                    continue
+                predecessor[next_pos] = cur_pos
+                queue.append(next_pos)
 
     return []
 
@@ -111,12 +119,13 @@ def astar(maze):
     """
     # TODO: Write your code here
 
-    from queue import PriorityQueue
+    import heapq as hq
 
     start_pos = maze.getStart()
 
-    frontier = PriorityQueue()
-    frontier.put(start_pos,0)
+    frontier = []
+    hq.heapify(frontier)
+    hq.heappush(frontier, (0,0,start_pos))
 
     
     predecessor = dict()
@@ -129,8 +138,8 @@ def astar(maze):
     goal_pos = goals[0]
     goal_row, goal_col = goal_pos
 
-    while not frontier.empty():
-        cur_pos = frontier.get()
+    while frontier:
+        cur_f, cur_q, cur_pos = hq.heappop(frontier)
         cur_row, cur_col = cur_pos
 
         if cur_pos in maze.getObjectives():
@@ -143,8 +152,9 @@ def astar(maze):
             temp_cost = cost[cur_pos] + abs(cur_row-next_row) + abs(cur_col-next_col)
             if next_pos not in cost or temp_cost < cost[next_pos]:
                 cost[next_pos] = temp_cost
+                q = temp_cost
                 f = temp_cost + abs(goal_row-next_row) + abs(goal_col-next_col)
-                frontier.put(next_pos,f)
+                hq.heappush(frontier,(f,q, next_pos))
                 predecessor[next_pos] = cur_pos
 
     path = []
