d89ceff9d709c55214fb0aae3869014f825eb6f4
Sean Yao <ckyao2@illinois.edu>
2020-09-23 17:26:01 -0500

    mp3 code skeleton

---
+++ mp3-code/naive_bayes.py
@@ -0,0 +1,33 @@
+# naive_bayes.py
+# ---------------
+# Licensing Information:  You are free to use or extend this projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to the University of Illinois at Urbana-Champaign
+#
+# Created by Justin Lizama (jlizama2@illinois.edu) on 09/28/2018
+
+"""
+This is the main entry point for MP4. You should only modify code
+within this file -- the unrevised staff files will be used for all other
+files and classes when code is run, so be careful to not modify anything else.
+"""
+
+def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter, pos_prior):
+    """
+    train_set - List of list of words corresponding with each movie review
+    example: suppose I had two reviews 'like this movie' and 'i fall asleep' in my training set
+    Then train_set := [['like','this','movie'], ['i','fall','asleep']]
+
+    train_labels - List of labels corresponding with train_set
+    example: Suppose I had two reviews, first one was positive and second one was negative.
+    Then train_labels := [1, 0]
+
+    dev_set - List of list of words corresponding with each review that we are testing on
+              It follows the same format as train_set
+
+    smoothing_parameter - The smoothing parameter you provided with --laplace (1.0 by default)
+    """
+    # TODO: Write your code here
+    # return predicted labels of development set
+    return []
