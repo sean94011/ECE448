4809975f8cd60cb3946a6f59173aa8d89f2ffa9f
Sean Yao <ckyao2@illinois.edu>
2020-09-27 15:34:23 -0500

    mp3

--- mp3-code/mp3.py
+++ mp3-code/mp3.py
@@ -14,46 +14,41 @@ import numpy as np
 
 import reader
 import naive_bayes as nb
+from sklearn.metrics import confusion_matrix
 
 """
 This file contains the main application that is run for this MP.
 """
 
-def compute_accuracies(predicted_labels, dev_set, dev_labels):
+def compute_accuracies(predicted_labels, dev_labels):
     yhats = predicted_labels
     accuracy = np.mean(yhats == dev_labels)
-    tp = np.sum([yhats[i] == dev_labels[i] and yhats[i] == 1 for i in range(len(yhats))])
-    precision = tp / np.sum([yhats[i] == 1 for i in range(len(yhats))])
-    recall = tp / (np.sum([yhats[i] != dev_labels[i] and yhats[i] == 0 for i in range(len(yhats))]) + tp)
-    f1 = 2 * (precision * recall) / (precision + recall)
-    return accuracy, f1, precision, recall
+    cm = confusion_matrix(dev_labels, predicted_labels)
+    tn, fp, fn, tp = cm.ravel()
+    true_negative = tn
+    false_positive = fp
+    false_negative = fn
+    true_positive = tp
+    return accuracy, false_positive, false_negative, true_positive, true_negative
 
 
 def main(args):
-    train_set, train_labels, dev_set, dev_labels = reader.load_dataset(args.training_dir,args.development_dir,args.stemming,args.lower_case)
-    predicted_labels = nb.naiveBayes(train_set, train_labels, dev_set, args.laplace, args.pos_prior)
+    #Modify stemming and lower case below. Note that our test cases may use both settings of the two parameters
+    train_set, train_labels, dev_set, dev_labels = reader.load_dataset(args.training_dir,args.development_dir,stemming=False,lower_case=False)
 
-    accuracy, f1, precision, recall = compute_accuracies(predicted_labels, dev_set, dev_labels)
+    predicted_labels = nb.naiveBayes(train_set, train_labels, dev_set)
+    accuracy, false_positive, false_negative, true_positive, true_negative = compute_accuracies(predicted_labels,dev_labels)
     print("Accuracy:",accuracy)
-    print("F1-Score:",f1)
-    print("Precision:",precision)
-    print("Recall:",recall)
-
+    print("False Positive", false_positive)
+    print("Fale Negative", false_negative)
+    print("True Positive", true_positive)
+    print("True Negative", true_negative)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='CS440 MP4 Naive Bayes')
-
+    parser = argparse.ArgumentParser(description='CS440 MP3 Naive Bayes')
     parser.add_argument('--training', dest='training_dir', type=str, default = '../data/movies_review/train',
                         help='the directory of the training data')
     parser.add_argument('--development', dest='development_dir', type=str, default = '../data/movies_review/dev',
                         help='the directory of the development data')
-    parser.add_argument('--stemming',dest="stemming", type=bool, default=False,
-                        help='Use porter stemmer')
-    parser.add_argument('--lower_case',dest="lower_case", type=bool, default=False,
-                        help='Convert all word to lower case')
-    parser.add_argument('--laplace',dest="laplace", type=float, default = 1.0,
-                        help='Laplace smoothing parameter - default 1.0')
-    parser.add_argument('--pos_prior',dest="pos_prior", type=float, default = 0.8,
-                        help='Positive prior, i.e. Num_positive_comments / Num_comments')
     args = parser.parse_args()
     main(args)
